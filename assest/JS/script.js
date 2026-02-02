cb8f6a54907de3163178cb7685c27db6e64e026d
Jalix-WebTech <jalixwebtech@gmail.com>
2026-02-02 19:29:19 +0100

    PAGES UPDATES

--- assest/JS/script.js
+++ assest/JS/script.js
@@ -28,3 +28,33 @@
   document.querySelectorAll('.fade-section').forEach((el) => {
     observer.observe(el);
   });
+
+  
+  // Functionn for contact us page animated video background section
+
+  // Main background animation
+lottie.loadAnimation({
+  container: document.getElementById("lottie-bg"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets10.lottiefiles.com/packages/lf20_totrpclr.json"
+});
+
+// Left box animation
+lottie.loadAnimation({
+  container: document.getElementById("lottie-left"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets2.lottiefiles.com/packages/lf20_x62chJ.json"
+});
+
+// Right box animation
+lottie.loadAnimation({
+  container: document.getElementById("lottie-right"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets9.lottiefiles.com/packages/lf20_5ngs2ksb.json"
+});
